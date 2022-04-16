8d61c5283bb8715829fabfa728c1a5c1e2801069
arashtajdar <arashtajdar@gmail.com>
2022-04-16 11:57:31 +0200

    Create BookCollection

---
+++ app/Http/Resources/BookCollection.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class BookCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            "Book name" => $this->name,
+            "Book author" => $this->author,
+            "creation date" => $this->created_at,
+        ];
+    }
+}
