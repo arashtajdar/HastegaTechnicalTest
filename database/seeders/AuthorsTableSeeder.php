a581a825b28c80f37d50773611d13451edd02620
arashtajdar <arashtajdar@gmail.com>
2022-04-16 10:26:41 +0200

    Create seeders

---
+++ database/seeders/AuthorsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AuthorsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->command->info('Authors table seeded!');
+
+    }
+}
