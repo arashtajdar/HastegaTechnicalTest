60b296ff94b8508de15935a4ddd19980cb3f0f8f
arashtajdar <arashtajdar@gmail.com>
2022-04-16 13:10:58 +0200

    Update BookCollection

--- app/Http/Resources/BookCollection.php
+++ app/Http/Resources/BookCollection.php
@@ -2,21 +2,30 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use JsonSerializable;
 
-class BookCollection extends ResourceCollection
+/**
+ * @property mixed name
+ * @property mixed author
+ * @property mixed created_at
+ */
+class BookCollection extends JsonResource
 {
     /**
      * Transform the resource collection into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     * @param  Request  $request
+     * @return array|Arrayable|JsonSerializable
      */
     public function toArray($request)
     {
         return [
             "Book name" => $this->name,
-            "Book author" => $this->author,
+            "Book author" => $this->author->name?$this->author->name:"No author",
             "creation date" => $this->created_at,
         ];
     }
