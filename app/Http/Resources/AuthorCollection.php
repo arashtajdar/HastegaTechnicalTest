1f137346b796ac0ba31ce2cd55553c7ae36ecc7e
arashtajdar <arashtajdar@gmail.com>
2022-04-16 11:53:45 +0200

    Create AuthorCollection

---
+++ app/Http/Resources/AuthorCollection.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class AuthorCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            "Author name" => $this->name,
+            "Creation date" => $this->created_at
+        ];
+    }
+}
