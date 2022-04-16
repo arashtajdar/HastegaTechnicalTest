eda6a546d408bfdf5125c0e1983054e10838a043
arashtajdar <arashtajdar@gmail.com>
2022-04-16 11:54:21 +0200

    Implement index method for AuthorController

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\AuthorCollection;
+use App\Models\Author;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthorController extends Controller
 {
     public function __construct()
     {
     }
+    /**
+     * Display a list of all Authors.
+     *
+     * @return Response
+     */
+    public function index(): Response
+    {
+        $authors = Author::all();
+        return Response(AuthorCollection::collection($authors),"200");
+    }
+
+
 
 }
